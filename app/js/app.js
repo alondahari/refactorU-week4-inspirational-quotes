4e51bb5b6df18d91169c28e79e9ed05dc9d95eae
Alon Dahari <climbinghobo@gmail.com>
2015-02-04 11:29:30 -0700

    seperate controllers

--- app/js/app.js
+++ app/js/app.js
@@ -6,50 +6,8 @@ var quoteApp = angular.module('quotes', ['ngRoute'])
         templateUrl: 'partials/quote-list.html',
         controller: 'QuoteListCtrl'
       })
-      .when('/author', {
-        templateUrl: 'partials/quote-list.html',
-        controller: 'QuoteListCtrl'
+      .when('/author/:author', {
+        templateUrl: 'partials/author-list.html',
+        controller: 'AuthorListCtrl'
       });
-  }])
-
-  .controller('QuoteListCtrl', ['$scope', function ($scope) {
-    
-    $scope.quotes = [
-      {
-        text: 'The bear necessities will come to you',
-        author: 'Baloo',
-        stars: 3
-      }
-    ];
-
-    $scope.predicate = 'stars';
-    $scope.reverse = true;
-
-    $scope.addQuote = function () {
-      $scope.quotes.push({
-        text: $scope.newQuoteText,
-        author: $scope.newQuoteAuthor,
-        stars: 1
-      });
-      $scope.newQuoteText = '';
-      $scope.newQuoteAuthor = '';
-    };
-
-    $scope.formatStars = function (num) {
-      var arr = new Array(5);
-      for (var i = 0; i < num; i++) {
-        arr[i] = 'star-filled';
-      }
-      return arr;
-    };
-
-    $scope.setStars = function (quote) {
-      quote.stars = this.$index + 1;
-    };
-
-    $scope.removeQuote = function(quote) {
-      $scope.quotes = $scope.quotes.filter(function(val) {
-        return val.$$hashKey != quote.$$hashKey;
-      });
-    }
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,43 +1,92 @@
-var quoteListControllers = angular.module('quoteListControllers', [])
-	.controller('QuoteListCtrl', ['$scope', '$http', function ($scope, $http) {
-		
-    $scope.quotes = [
-      {
-        text: 'The bear necessities will come to you',
-        author: 'Baloo',
-        stars: 3
-      }
-    ];
+quoteApp.controller('Quotes', ['$scope', function ($scope) {
 
-    $scope.predicate = 'stars';
-    $scope.reverse = true;
-
-    $scope.addQuote = function () {
-      $scope.quotes.push({
-        text: $scope.newQuoteText,
-        author: $scope.newQuoteAuthor,
-        stars: 1
-      });
-      $scope.newQuoteText = '';
-      $scope.newQuoteAuthor = '';
-    };
-
-    $scope.formatStars = function (num) {
-      var arr = new Array(5);
-      for (var i = 0; i < num; i++) {
-        arr[i] = 'star-filled';
-      }
-      return arr;
-    };
-
-    $scope.setStars = function (quote) {
-      quote.stars = this.$index + 1;
-    };
-
-    $scope.removeQuote = function(quote) {
-      $scope.quotes = $scope.quotes.filter(function(val) {
-        return val.$$hashKey != quote.$$hashKey;
-      });
+  $scope.quotes = [
+    {
+      text: 'The bear necessities will come to you',
+      author: 'Baloo',
+      stars: 3
     }
-	}]);
+  ];
+
+  $scope.predicate = 'stars';
+  $scope.reverse = true;
+
+  $scope.formatStars = function (num) {
+    var arr = new Array(5);
+    for (var i = 0; i < num; i++) {
+      arr[i] = 'star-filled';
+    }
+    return arr;
+  };
+
+  $scope.setStars = function (quote) {
+    quote.stars = this.$index + 1;
+  };
+
+  $scope.removeQuote = function(quote) {
+    $scope.quotes = $scope.quotes.filter(function(val) {
+      return val.$$hashKey != quote.$$hashKey;
+    });
+  };
+
+}]);
+
+
+
+quoteApp.controller('QuoteListCtrl', ['$scope', function ($scope) {
+
+  $scope.addQuote = function () {
+    $scope.quotes.push({
+      text: $scope.newQuoteText,
+      author: $scope.newQuoteAuthor,
+      stars: 1
+    });
+    $scope.newQuoteText = '';
+    $scope.newQuoteAuthor = '';
+  };
+}]);
+
+
+quoteApp.controller('AuthorListCtrl', ['$scope', function ($scope) {
+	
+  $scope.quotes = [
+    {
+      text: 'The bear necessities will come to you',
+      author: 'Baloo',
+      stars: 3
+    }
+  ];
+
+  $scope.predicate = 'stars';
+  $scope.reverse = true;
+
+  $scope.addQuote = function () {
+    $scope.quotes.push({
+      text: $scope.newQuoteText,
+      author: $scope.newQuoteAuthor,
+      stars: 1
+    });
+    $scope.newQuoteText = '';
+    $scope.newQuoteAuthor = '';
+  };
+
+  $scope.formatStars = function (num) {
+    var arr = new Array(5);
+    for (var i = 0; i < num; i++) {
+      arr[i] = 'star-filled';
+    }
+    return arr;
+  };
+
+  $scope.setStars = function (quote) {
+    quote.stars = this.$index + 1;
+  };
+
+  $scope.removeQuote = function(quote) {
+    $scope.quotes = $scope.quotes.filter(function(val) {
+      return val.$$hashKey != quote.$$hashKey;
+    });
+  };
+}]);
+
 
