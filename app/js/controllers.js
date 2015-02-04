da87d09fe1bfe816c02ff6b5cd7d3668896cb96c
Alon Dahari <climbinghobo@gmail.com>
2015-02-04 14:00:35 -0700

    add random quote

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,8 +1,8 @@
-quoteApp.controller('Quotes', ['$scope', function ($scope) {
+quoteApp.controller('QuotesCtrl', ['$scope', function ($scope) {
 
   $scope.quotes = [
     {
-      text: 'The bear necessities will come to you',
+      text: 'The bear necessities in life will come to you',
       author: 'Baloo',
       stars: 3
     }
@@ -27,6 +27,15 @@ quoteApp.controller('Quotes', ['$scope', function ($scope) {
     $scope.quotes = $scope.quotes.filter(function(val) {
       return val.$$hashKey != quote.$$hashKey;
     });
+  };
+
+  $scope.popRandomQuote = function(){
+    $scope.randomQuote = _.sample($scope.quotes);
+  };
+
+  $scope.hideLightbox = function(){
+    $scope.randomQuote = null;
+
   };
 
 }]);
@@ -47,46 +56,17 @@ quoteApp.controller('QuoteListCtrl', ['$scope', function ($scope) {
 }]);
 
 
-quoteApp.controller('AuthorListCtrl', ['$scope', function ($scope) {
-	
-  $scope.quotes = [
-    {
-      text: 'The bear necessities will come to you',
-      author: 'Baloo',
-      stars: 3
-    }
-  ];
-
-  $scope.predicate = 'stars';
-  $scope.reverse = true;
-
-  $scope.addQuote = function () {
+quoteApp.controller('AuthorListCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+	$scope.author = $routeParams.author;
+    $scope.addQuote = function () {
     $scope.quotes.push({
       text: $scope.newQuoteText,
-      author: $scope.newQuoteAuthor,
+      author: $scope.author,
       stars: 1
     });
     $scope.newQuoteText = '';
-    $scope.newQuoteAuthor = '';
   };
 
-  $scope.formatStars = function (num) {
-    var arr = new Array(5);
-    for (var i = 0; i < num; i++) {
-      arr[i] = 'star-filled';
-    }
-    return arr;
-  };
-
-  $scope.setStars = function (quote) {
-    quote.stars = this.$index + 1;
-  };
-
-  $scope.removeQuote = function(quote) {
-    $scope.quotes = $scope.quotes.filter(function(val) {
-      return val.$$hashKey != quote.$$hashKey;
-    });
-  };
 }]);
 
 
